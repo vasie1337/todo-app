e1eaf45db7605fef9c76de59f62de27276bf97fa
vasie <59412971+vasie1337@users.noreply.github.com>
2025-06-05 16:34:30 +0200

    Refactor main function to improve database error handling and define APP_PORT constant for better readability

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use actix_cors::Cors;
 mod routes;
 mod db;
 
+const APP_PORT: u16 = 8080;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let db = db::DataBase::open("db.sql").expect("Database err");
-    db.migrate();
-
-    let port: u16 = 8080;
+    let db = db::DataBase::open("db.sql").expect("Failed to open the database");
+    db.migrate().expect("Failed to migrate the database");
 
     HttpServer::new(move || {
         App::new()
@@ -23,7 +23,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(actix_web::web::Data::new(db.clone()))
             .configure(routes::config)
     })
-    .bind(("0.0.0.0", port))?
+    .bind(("0.0.0.0", APP_PORT))?
     .run()
     .await
 }
